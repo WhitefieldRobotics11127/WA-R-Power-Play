81c4b25de924846267bf2d0a005e4329a8b2541a
MarinEckerson <mve5002@outlook.com>
2022-11-19 09:14:40 -0500

    Finalized the grabber code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PwrPlay_Rover.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PwrPlay_Rover.java
@@ -179,24 +179,36 @@ public class PwrPlay_Rover extends OpMode {
         robot.dcMotor5.setPower(gamepad2.right_stick_y);
         robot.dcMotor6.setPower(gamepad2.right_stick_y);
 
+        //Bumpers open/close the 2 sides simultaneously
+        if (gamepad2.left_bumper){
+            rotisseriePos = PowerPlayPackBot.rotisserieOpen;
+            chickenPos = PowerPlayPackBot.chickenOpen;
+        }
+        if (gamepad2.right_bumper){
+            rotisseriePos = PowerPlayPackBot.rotisserieClosed;
+            chickenPos = PowerPlayPackBot.chickenClosed;
+        }
+
+        //Buttons open/close them separately
         if (gamepad2.a){
-            //chickenPos = PowerPlayPackBot.chickenOpen;
-            robot.chicken.setPosition(PowerPlayPackBot.chickenOpen);
+            chickenPos = PowerPlayPackBot.chickenOpen;
+            //robot.chicken.setPosition(PowerPlayPackBot.chickenOpen);
         }
         if (gamepad2.b){
-            //chickenPos = PowerPlayPackBot.chickenClosed;
-            robot.chicken.setPosition(PowerPlayPackBot.chickenClosed);
+            chickenPos = PowerPlayPackBot.chickenClosed;
+            //robot.chicken.setPosition(PowerPlayPackBot.chickenClosed);
         }
         if (gamepad2.x){
-            //rotisseriePos = PowerPlayPackBot.rotisserieOpen;
-            robot.rotisserie.setPosition(PowerPlayPackBot.rotisserieOpen);
+            rotisseriePos = PowerPlayPackBot.rotisserieOpen;
+            //robot.rotisserie.setPosition(PowerPlayPackBot.rotisserieOpen);
         }
         if (gamepad2.y){
-            //rotisseriePos = PowerPlayPackBot.rotisserieClosed;
-            robot.rotisserie.setPosition(PowerPlayPackBot.rotisserieClosed);
+            rotisseriePos = PowerPlayPackBot.rotisserieClosed;
+            //robot.rotisserie.setPosition(PowerPlayPackBot.rotisserieClosed);
         }
-        //robot.rotisserie.setPosition(rotisseriePos);
-        //robot.chicken.setPosition(chickenPos);
+
+        robot.rotisserie.setPosition(rotisseriePos);
+        robot.chicken.setPosition(chickenPos);
 
 //        if (gamepad2.left_stick_button)
 //            lifty_speed = .3;
